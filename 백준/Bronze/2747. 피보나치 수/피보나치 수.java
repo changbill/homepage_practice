168c018569b35ca0f9affe9daa5a5b0455baf2fd
changbill <128214736+changbill@users.noreply.github.com>
2023-08-04 17:13:46 +0900

    [Bronze II] Title: 피보나치 수, Time: 140 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int fibo1 = 0;
+		int fibo2 = 1;
+		int save = 0;
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		if (N>1) {
+			for (int i=0; i<N; i++) {
+				save = fibo2;
+				fibo2 += fibo1;
+				fibo1 = save;
+			}
+		} else if (N==1) {
+			fibo1 = 1;
+		} 
+		
+		System.out.println(fibo1);
+	}
+}
